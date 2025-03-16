3b086c7edc3a5d4afd47697fb0c0c04e147a2948
RodrigoKartcheki <rodrigo.kartcheski@gmail.com>
2025-03-16 20:03:32 -0300

    function

--- cloudrun/function_labs/lab1/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-
-from flask import Flask, request
-
-app = Flask(__name__)
-
-def home(request):
-    """Ponto de entrada para o Cloud Functions."""
-    return "Hello, Cloud Functions!", 200

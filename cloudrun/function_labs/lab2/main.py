324b2c66cfde59e7c428336e0c8dc022841ed913
RodrigoKartcheki <rodrigo.kartcheski@gmail.com>
2025-03-17 11:19:01 -0300

    lab2

---
+++ cloudrun/function_labs/lab2/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import functions_framework
+from gcs2bq import GcsToDataFrame, BigQueryLoader
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def hello():
+   # Parâmetros necessários
+    project_id = "dataplex-experience-6133"
+    bucket_name = "bucket_new2025"
+    folder_name = "pastateste"
+    sheet_name = "Planilha1"
+    dataset_id = "data_conjunto"
+    table_id = "table_id2"
+    unique_key_columns = ["id"]
+
+    # Carregar os dados do GCS usando a classe GcsToDataFrame
+    gcs_loader = GcsToDataFrame(
+        project_id=project_id,
+        bucket_name=bucket_name,
+        folder_name=folder_name,
+        sheet_name=sheet_name
+    )
+
+    # Chama o método que retorna o DataFrame com os dados carregados
+    df = gcs_loader.load_all_excel_from_folder()
+
+    # Chamar a classe BigQueryLoader e carregar os dados no BigQuery
+    bq_loader = BigQueryLoader(project_id, dataset_id, table_id, unique_key_columns)
+    bq_loader.upsert_to_bigquery(df)
+
+    return "Data loaded to BigQuery successfully", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
